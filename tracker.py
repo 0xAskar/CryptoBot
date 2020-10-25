a3489afb5ffe72fcff91e896288743c652aaf4bf
mattaska <mattaska@umich.edu>
2020-10-25 12:44:28 -0400

    bot now instead of client

--- tracker.py
+++ tracker.py
@@ -1,7 +1,7 @@
 client_secret = "arlLaG80mW6GgG7cpQ7re_0zfbuZIqfI"
 client_id = 769265839584051200
 public_key = "50d8dbb3ff26eec310978d56a17705489eeee4882f65d7034019598998bc8ea6"
-bot_token = 'NzY5MjY1ODM5NTg0MDUxMjAw.X5MgpA.koCz6kt4uLFonz80eZdmEllarQ0'
+bot_token = 'NzY5MjY1ODM5NTg0MDUxMjAw.X5MgpA.NJQe7HIVJcdHyXuVH9S8NvUyWSM'
 guild = "Playground"
 
 
@@ -18,48 +18,90 @@ from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 import sys
 from coinmarketcapapi import CoinMarketCapAPI, CoinMarketCapAPIError
-
+from discord.ext import commands
 
 cmc = CoinMarketCapAPI(cmc_key)
 m = cmc.cryptocurrency_listings_latest()
 ticker = "NONE"
 load_dotenv()
+bot = commands.Bot(command_prefix='!')
 # Token = os.getenv('DISCORD_TOKEN')
 # print(Token)
 
 # establish client connection to discord api
-client = discord.Client()
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord!')
 
-@client.event
-async def on_message(message):
+# @bot.event
+# async def on_message(message):
+#
+#     link = "Here is the link to Trendy, do yourself a favor: https://www.youtube.com/channel/UCJpDaUsJ-IpvpDfH-AqZfcQ?view_as=subscriber"
+#
+#     info = message.content
+#     info = info.lower()
+#     if message.author == bot.user:
+#         return
+#
+#     if info == "!link":
+#         response = link
+#         await message.channel.send(response)
+#     elif info == "!bitcoin" or info == '!btc':
+#         price = m.data[0]['quote']['USD']['price']
+#         price = round(price,2)
+#         percent_change = m.data[0]['quote']['USD']['percent_change_24h']
+#         percent_change = round(percent_change, 2)
+#         response = "Bitcoin's price: $" + str(price) + ", Percent Change: " + str(percent_change) + "%"
+#         await message.channel.send(response)
+#     elif info == "!ethereum" or info == '!eth':
+#         price = m.data[1]['quote']['USD']['price']
+#         percent_change = m.data[1]['quote']['USD']['percent_change_24h']
+#         percent_change = round(percent_change, 2)
+#         price = round(price,2)
+#         response = "Ethereum's price: $" + str(price) + ", Percent Change: " + str(percent_change) + "%"
+#         await message.channel.send(response)
 
-    link = "Here is the link to Trendy, do yourself a favor: https://www.youtube.com/channel/UCJpDaUsJ-IpvpDfH-AqZfcQ?view_as=subscriber"
+def bitcoin_output():
+    price = m.data[0]['quote']['USD']['price']
+    price = round(price,2)
+    percent_change = m.data[0]['quote']['USD']['percent_change_24h']
+    percent_change = round(percent_change, 2)
+    response = "Bitcoin's price: $" + str(price) + ", Percent Change: " + str(percent_change) + "%"
+    return response
+    
+def ethereum_output():
+    price = m.data[1]['quote']['USD']['price']
+    percent_change = m.data[1]['quote']['USD']['percent_change_24h']
+    percent_change = round(percent_change, 2)
+    price = round(price,2)
+    response = "Ethereum's price: $" + str(price) + ", Percent Change: " + str(percent_change) + "%"
+    return reponse
 
-    info = message.content
-    info = info.lower()
-    if message.author == client.user:
-        return
+@bot.command(name = "bitcoin")
+async def btc_price(message):
+    await message.channel.send(bitcoin_output())
+@bot.command(name = "btc")
+async def btc_price(message):
+    await message.channel.send(bitcoin_output())
+@bot.command(name = "BTC")
+async def btc_price(message):
+    await message.channel.send(bitcoin_output())
+@bot.command(name = "Bitcoin")
+async def btc_price(message):
+    await message.channel.send(bitcoin_output())
 
-    if info == "!link":
-        response = link
-        await message.channel.send(response)
-    elif info == "!bitcoin" or info == '!btc':
-        price = m.data[0]['quote']['USD']['price']
-        price = round(price,2)
-        percent_change = m.data[0]['quote']['USD']['percent_change_24h']
-        percent_change = round(percent_change, 2)
-        response = "Bitcoin's price: $" + str(price) + ", Percent Change: " + str(percent_change) + "%"
-        await message.channel.send(response)
-    elif info == "!ethereum" or info == '!eth':
-        price = m.data[1]['quote']['USD']['price']
-        percent_change = m.data[1]['quote']['USD']['percent_change_24h']
-        percent_change = round(percent_change, 2)
-        price = round(price,2)
-        response = "Ethereum's price: $" + str(price) + ", Percent Change: " + str(percent_change) + "%"
-        await message.channel.send(response)
+@bot.command(name = "ethereum")
+async def btc_price(message):
+    await message.channel.send(bitcoin_output())
+@bot.command(name = "eth")
+async def btc_price(message):
+    await message.channel.send(bitcoin_output())
+@bot.command(name = "ETH")
+async def btc_price(message):
+    await message.channel.send(bitcoin_output())
+@bot.command(name = "Ethereum")
+async def btc_price(message):
+    await message.channel.send(bitcoin_output())
 
-client.run(bot_token) # runs forever
+bot.run(bot_token) # runs forever
