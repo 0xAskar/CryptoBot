91b3af35c409b6fcaddd8792cec2006be3879b0e
Matthew <>
2020-11-25 02:43:18 -0500

    attempting to use gitignore

--- bot_ids.py
+++ bot_ids.py
@@ -1,4 +1,4 @@
-bot_token_real = "NzY5MjY1ODM5NTg0MDUxMjAw.X5MgpA.5CpVoO06a69Vx4O_BzLjt_ogYvo"
+bot_token_real = "NzY5MjY1ODM5NTg0MDUxMjAw.X5MgpA.KW89WiYj0u4pZOf-a_g33LzQnH0"
 bot_id_real= 769265839584051200
-bot_token_tester = "Nzc4ODMzNDczOTEzMjI1MjE2.X7XvMg.bG0Q7JV-FM3FJv5_k8esqrhVkbE"
+bot_token_tester = "Nzc4ODMzNDczOTEzMjI1MjE2.X7XvMg.HErR_zyfU_rLgKpk9HS1B5rTNQ4"
 bot_id_tester = 778833473913225216
