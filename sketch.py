f0e030a0ee652536d5fd31024dd9edb3883bc2fa
Matthew <>
2020-11-25 02:50:49 -0500

    cleaned up git and removed most files

--- sketch.py
+++
@@ -1,5 +0,0 @@
-import pathlib
-from pycoingecko import CoinGeckoAPI
-print(pathlib.Path().absolute())
-cg = CoinGeckoAPI()
-print(cg.get_coin_info_from_contract_address_by_id(id = "ethereum", contract_address = "0x34fba4fedac025547777975087707579746f305cd219c1645ea2a837e9e72d1a"))
